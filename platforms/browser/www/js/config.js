5b175d676762a90c956588589869271e13f1abf9
Sherwin Jupiter <jupiter@pivotmobileapp.com>
2017-03-29 12:30:41 -0400

    first

---
+++ platforms/browser/www/js/config.js
@@ -0,0 +1,6 @@
+var krms_config ={			
+	'ApiUrl' : "http://fellybellyapp.com/merchantapp/api",	
+	'DialogDefaultTitle' : "FellyBelly Manager",
+	'pushNotificationSenderid' : "629248534750",
+	'APIHasKey' : "fed7b441b349bae8f146711fbd215e90"
+};
